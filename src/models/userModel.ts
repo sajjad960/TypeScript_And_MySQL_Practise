ce2072553eaf226e20c105d753d84bcd99f0a0eb
sajjad960 <sajjadhossainsb01@gmail.com>
2022-04-13 22:14:28 +0600

    user validation added

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -8,19 +8,45 @@ const User = sequelize.define('user', {
     name: {
         type: DataTypes.STRING,
         allowNull: false,
+        validate: {
+            notNull: {
+                msg: 'Please tell us your name',
+            },
+        }
     },
-    email: { type: DataTypes.STRING, allowNull: false, unique: true },
+    email: {
+        type: DataTypes.STRING, allowNull: false, unique: true,
+        validate: {
+            isEmail: {
+                msg: 'Not a valid email address',
+            },
+            notNull: {
+                msg: 'Email is required',
+            },
+        }
+    },
     role: {
         type: DataTypes.ENUM,
-        values: ['admin', 'moderator', 'user']
+        values: ['admin', 'moderator', 'user'],
+        defaultValue: 'user',
     },
-    password: DataTypes.STRING,
+    password: {
+        type: DataTypes.STRING,
+        validate: {
+            notNull: {
+                msg: 'Please Provide a password',
+            },
+        }
+    },
     passwordConfirm: {
         type: DataTypes.STRING,
         validate: {
-            validator: function (el: any) {
+            function(el: any) {
                 return el === this.password
-            }
+            },
+            notNull: {
+                msg: 'Please Confirm Your Password',
+            },
         }
 
     },
