eb0fc74d7e0e218a777183284d33b98c790b311f
sajjad960 <sajjadhossainsb01@gmail.com>
2022-04-13 20:13:38 +0600

    created user model

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,20 +1,43 @@
-const { Sequelize, DataTypes } = require('sequelize');
+import { Sequelize, DataTypes } from 'sequelize';
+import validator from 'validator';
+
 const sequelize = new Sequelize;
+
 
 const User = sequelize.define('user', {
     name: {
         type: DataTypes.STRING,
         allowNull: false,
     },
-    email: { type: DataTypes.STRING, allowNull: false, },
+    email: { type: DataTypes.STRING, allowNull: false, unique: true },
     role: {
         type: DataTypes.ENUM,
         values: ['admin', 'moderator', 'user']
     },
     password: DataTypes.STRING,
     passwordConfirm: {
-        type: String,
+        type: DataTypes.STRING,
+        validate: {
+            validator: function (el: any) {
+                return el === this.password
+            }
+        }
 
-    }
+    },
+    passwordChangedAt: {
+        type: DataTypes.DATE,
+    },
+    passwordResetToken: DataTypes.STRING,
+    passwordResetExpires: DataTypes.DATE,
+    status: DataTypes.INTEGER,
+    cratedAt: {
+        type: DataTypes.DATE,
+        defaultValue: Date.now
+    },
+    updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+    },
+
 });
 
